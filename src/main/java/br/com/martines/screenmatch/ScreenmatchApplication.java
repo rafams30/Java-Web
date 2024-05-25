e948467ff798c19f36efe31e2f1b8adcda84ee6a
Rafael Martines <rafams_22@hotmail.com>
2024-05-25 19:11:40 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/martines/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,23 @@
+package br.com.martines.screenmatch;
+
+import br.com.martines.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		//System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
---
+++ src/main/java/br/com/martines/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.martines.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons")Integer totalTemporadas,
+                         @JsonAlias("imdbRating")String avaliacao,
+                         @JsonProperty("imdbVotes") String votos ) {
+}
+
