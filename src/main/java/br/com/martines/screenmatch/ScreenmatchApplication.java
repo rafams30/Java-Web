a89faee6475589ada383516a012bd0eb968b59c1
Rafael Martines <rafams_22@hotmail.com>
2024-05-25 19:35:24 -0300

    Modelando dados da série

--- src/main/java/br/com/martines/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/martines/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.martines.screenmatch;
 
+import br.com.martines.screenmatch.model.DadosSerie;
 import br.com.martines.screenmatch.service.ConsumoApi;
+import br.com.martines.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,5 +21,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		//System.out.println(json);
 		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
--- src/main/java/br/com/martines/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/martines/screenmatch/model/DadosSerie.java
@@ -1,11 +1,13 @@
 package br.com.martines.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons")Integer totalTemporadas,
-                         @JsonAlias("imdbRating")String avaliacao,
-                         @JsonProperty("imdbVotes") String votos ) {
+                         @JsonAlias("imdbRating")String avaliacao) {
+
 }
 
